f67c07a0a7d85326e896ec81e16a32a4ce6711b3
intheloop <admin@tiscacatalin.com>
2025-04-10 16:34:29 +0300

    got started with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::{arg, command, ArgAction};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .subcommand(
+            command!("init")
+                .about("initializes a kanban project in the current directory")
+                .arg(arg!(-n --name "name").action(ArgAction::SetTrue))
+                .arg(arg!(-d --description "description").action(ArgAction::SetTrue))
+                .arg(arg!(-a --author "author").action(ArgAction::SetTrue))
+                .arg(arg!(-y --yes "yes").action(ArgAction::SetTrue))
+        )
+        .get_matches();
 }
